05c43b113d9d9d1f7b1c6ebb6e9bcca85a28a548
jfcarocota <jfcarocota@gmail.com>
2018-10-24 00:17:08 -0700

    commit 3
    agregamos el menu basico de palma

---
+++ Assets/_Scripts/PalmMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PalmMenu : MonoBehaviour
+{
+    protected float palmRotZ;
+
+    [SerializeField]
+    float maxPalmRotZ = 0;
+    [SerializeField]
+    float minPalmRotZ = 0;
+
+    [SerializeField]
+    GameObject myMenu;
+
+    [SerializeField]
+    Transform palmRotTrs;
+
+    private void Start()
+    {
+        myMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        palmRotZ = palmRotTrs.rotation.z;
+        myMenu.SetActive(palmRotZ < maxPalmRotZ && palmRotZ > minPalmRotZ);
+        Debug.Log(palmRotZ);
+    }
+}
